53c74d2c0ff7ce04841c8557c411fa195c267f69
ssgadient <sgadient@ucsd.edu>
2026-03-15 06:33:59 -0700

    fix admin 403

--- reporting.cse135-css.site/public_html/api/admin/index.php
+++ reporting.cse135-css.site/public_html/api/admin/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'super_admin') {
-    header("Location: /");
+    http_response_code(403);
+    include('../../error_pages/403.html');
     exit();
 }
 ?>
