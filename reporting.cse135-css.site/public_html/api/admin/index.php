9ab67d1c6c67a0cf8b8d92c193a5c7567ada6fb4
ssgadient <sgadient@ucsd.edu>
2026-03-15 18:36:43 -0700

    admin 401 before 403

--- reporting.cse135-css.site/public_html/api/admin/index.php
+++ reporting.cse135-css.site/public_html/api/admin/index.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'super_admin') {
+if (!isset($_SESSION['user_id'])) {
+    http_response_code(401);
+    echo json_encode(["error" => "Unauthorized"]);
+    exit();
+}
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'super_admin') {
     http_response_code(403);
     include('../../error_pages/403.html');
     exit();
